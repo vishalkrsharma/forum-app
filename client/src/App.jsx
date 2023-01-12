76ddcdc19b714b8b4c9849b38faef34269a076ee
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-12 23:38:37 +0530

    export index created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Title from './pages/Title';
+import { Home, Signin, Signup, Title } from './pages/index';
 
 function App() {
   return (
