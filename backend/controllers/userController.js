f2ed01e13cfb98463841714a7adbb9f3546184f6
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-19 20:25:43 +0530

    setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,65 @@
+
+const User = require('../models/userModel')
+const UserToken = require('../models/userToken')
+const jwt = require('jsonwebtoken')
+
+
+const createToken = (payload,_key,expire)=>{
+
+    return jwt.sign(payload,_key,expire)
+}
+
+const loginUser = async (req,res)=>{
+    const {email,password} = req.body
+    
+    try{
+            const obj=await User.login(email,password)
+            console.log(obj)
+         
+            //create jwttoken
+            
+            const accessToken = createToken({'email':email,'password':password},process.env.SECRET_KEY,{expiresIn:'2d'})
+            const refreshToken = createToken({'email':email},process.env.REFRESH_KEY,{expiresIn:'30d'}) 
+            
+            //register to userToken
+            
+            const message = await UserToken.registerToken(obj,refreshToken)
+            res.status(200).json({
+                error:false,
+                message,
+                accessToken,
+                refreshToken
+            })
+        
+    }catch(error){
+        res.status(400).json({error:true,error:error.message})
+    }
+}
+// signup user
+const signupUser = async (req,res)=>{
+    const {email,password,username} = req.body
+    console.log(email,password,username)
+    try{
+        await User.signup(email,username,password)
+        res.status(200).json({error:false,message:"Registered Successfully"})   
+    }catch(error){
+        res.status(400).json({error:true,message:error.message})
+    }
+}
+const verifyToken = async (req ,res)=>{
+    const {refToken}=req.body;
+    try{
+        const status = UserToken.verifyToken(refToken)
+        
+        if(status){
+             jwt.verify(refToken,process.env.REFRESH_KEY,(err,user)=>{
+                if(err) throw Error('Cannot verify')
+                const accessToken = createToken({email: user.email,'password':user.password},process.env.SECRET_KEY,{expiresIn:'2d'})
+                res.status(200).json({error:false,message:"Success",accessToken:accessToken})
+            });
+        }
+    }catch(err){
+        res.status(400).json({error:true,message:err.message})
+    }
+}
+module.exports = {signupUser,loginUser,verifyToken}
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
+
+//app
+const app = express();
+
+//database connect
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected!!'))
+  .catch((err) => console.log('DB CONNECTION ERROR', err));
+
+//middleware
+app.use(morgan('dev'));
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+
+//routes
+app.use('/api/user', userRoutes);
+app.use('/post', postRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log('listening on port 8000!!');
+});
