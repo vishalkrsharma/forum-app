df8c317eb156dbd4789e2c3446a7e69b3081b5fd
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-20 18:32:43 +0530

    new post button fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import { MdPostAdd } from 'react-icons/md';
 
 import { TopBar } from '../components/index';
 
 export default function Home() {
+  const location = useLocation();
+  console.log(location.pathname);
+
   return (
     <div className='h-screen'>
       <TopBar />
       <Outlet />
-      <Link to='/newpost' className='bg-primary p-4 rounded-2xl text-white fixed bottom-4 right-4 flex justify-center items-center gap-2 drop-shadow-2xl'>
-        <MdPostAdd className='text-3xl' />
-        New Post
-      </Link>
+      {location.pathname === '/newpost' ? null : (
+        <Link to='/newpost' className='bg-primary p-4 rounded-2xl text-white fixed bottom-4 right-4 flex justify-center items-center gap-2 drop-shadow-2xl'>
+          <MdPostAdd className='text-3xl' />
+          New Post
+        </Link>
+      )}
     </div>
   );
 }
--- client/src/pages/NewPost.jsx
+++ client/src/pages/NewPost.jsx
@@ -13,7 +13,7 @@ export default function NewPost() {
 
   return (
     <div>
-      <div className='bg-diffused py-20 px-4 h-screen'>
+      <div className='py-20 px-4 h-screen'>
         <form className='flex flex-col justify-start gap-4'>
           <label className='text-black font-medium -mb-3' htmlFor='title' style={{ marginLeft: '.7rem' }}>
             Give a title...
