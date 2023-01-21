51732e70eed9611cb962485d9f74ed48c7fbd397
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-21 14:22:48 +0530

    proxy and routes fixed

--- client/src/hooks/useSignup.jsx
+++ client/src/hooks/useSignup.jsx
@@ -7,26 +7,26 @@ import useLogin from './useLogin';
 export default function useSignup() {
   const [error, setError] = useState(null);
   const [isLoading, setLoading] = useState(null);
-  const {login} = useLogin();
+  const { login } = useLogin();
   const { dispatch } = useAuthContext();
 
   const signup = async (body) => {
     setLoading(true);
     setError(null);
     try {
-      const { data } = await axios.post('http://localhost:8000/api/user/register', body).then((res)=>{
-        if(res.status) login(body)
+      const { data } = await axios.post('/api/user/register', body).then((res) => {
+        if (res.status) login(body);
       });
       setLoading(false);
 
       //dispatch({ type: 'LOGIN', payload: data });
       //localStorage.setItem('user', JSON.stringify(data));
-      return 
+      return;
     } catch (err) {
       setError(err);
       console.log(err);
     }
   };
 
-  return {error,isLoading,signup};
+  return { error, isLoading, signup };
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host : true,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:8000/',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
   },
 });
