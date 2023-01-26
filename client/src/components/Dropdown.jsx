4c8af5bd14a84325ed234d97b181f9c6fa6e25bb
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-01-26 20:57:01 +0530

    navbar css changes

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -33,17 +33,20 @@ export default function Dropdown() {
           <AiOutlineUser />
         </button>
         {showMenu ? (
-          <div className='dropdown__menu bg-white absolute top-12 right-0 rounded-lg shadow-lg text-base z-10'>
-            <Link to='/profile' className='item py-2 px-5 flex justify-start items-center gap-2 '>
-              <AiOutlineUser />
+          <div className='dropdown__menu bg-white absolute top-12 -right-3 rounded-lg shadow-lg text-base z-10 w-48'>
+            <Link to='/profile' className='item py-2 px-5 flex justify-start items-center gap-4 hover:bg-primary hover:text-white m-2 rounded-lg'>
+              <AiOutlineUser className='text-2xl' />
               Profile
             </Link>
-            <Link to='/settings' className='item py-2 px-5 flex justify-start items-center gap-2'>
-              <AiOutlineSetting />
+            <Link to='/settings' className='item py-2 px-5 flex justify-start items-center gap-4 hover:bg-primary hover:text-white m-2 rounded-lg'>
+              <AiOutlineSetting className='text-2xl' />
               Settings
             </Link>
-            <div className='item py-2 px-5 flex justify-start items-center gap-2 cursor-pointer' onClick={logoutHandler}>
-              <AiOutlineLogout />
+            <div
+              className='item py-2 px-5 flex justify-start items-center gap-4 hover:bg-primary hover:text-white m-2 rounded-lg cursor-pointer'
+              onClick={logoutHandler}
+            >
+              <AiOutlineLogout className='text-2xl' />
               Log Out
             </div>
           </div>
