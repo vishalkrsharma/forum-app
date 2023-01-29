1b524fa04ebff9bc1ef8b01c875d03eca21782b4
vishalkrsharma <vishalkrsharm1234@gmail.com>
2023-01-29 23:06:09 +0530

    profile page design

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { default as BoringAvatar } from 'boring-avatars';
 
-export default function Avatar({ name, variant }) {
-  return <BoringAvatar size={40} name={name} variant={variant} colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']} />;
+export default function Avatar({ name, variant, size, square }) {
+  return <BoringAvatar size={size} name={name} variant={variant} square={square} colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']} />;
 }
