815f5e8d64f34b6dba7e1321ea7ce33bb664ab5d
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-02-17 14:06:30 +0530

    verifymail signup css changes

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -50,8 +50,10 @@ export default function Signup({ route }) {
   };
 
   return (
-    <div className='mx-auto h-screen p-3 bg-bg'>
-      <img className='h-44 mx-auto' src={logoWithLabel} alt={logoWithLabel} />
+    <div className='mx-auto h-screen max-lg:p-3 lg:flex lg:justify-center lg:items-center'>
+      <div className='lg:h-full lg:w-1/2 lg:bg-bg flex justify-center items-center max-lg:mb-5'>
+        <img className='h-56 p-4 bg-white rounded-full' src={logoWithLabel} alt={logoWithLabel} />
+      </div>
       <form className='mx-auto flex justify-top items-center flex-col gap-7 w-80'>
         <div className='text-xl text-primary font-medium -mb-5'>Sign Up</div>
         <div className='relative w-full'>
@@ -130,14 +132,13 @@ export default function Signup({ route }) {
         <button className='bg-primary text-white w-full h-10 rounded-lg mt-4 disabled:bg-secondary cursor-pointer' type='submit' onClick={submitHandler}>
           Sign Up
         </button>
+        <div className='w-80 mx-auto text-center mt-5'>
+          Already have an account!&nbsp;&nbsp;
+          <Link className='text-secondary hover:text-primary' to={'/login'}>
+            Log In
+          </Link>
+        </div>
       </form>
-
-      <div className='w-80 mx-auto text-center mt-5'>
-        Already have an account!&nbsp;&nbsp;
-        <Link className='text-secondary hover:text-primary' to={'/login'}>
-          Log In
-        </Link>
-      </div>
     </div>
   );
 }
