d41cf239e06d316b6091b1f5286c3b77ad8134b7
samwilson0745 <sohamdeb0745@gmail.com>
2023-03-06 12:47:04 +0530

    postdropdown changed

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -3,11 +3,12 @@ import { Avatar } from "./index";
 import { formatDistance } from "date-fns";
 import PostDropdown from "./PostDropdown";
 import { BiLike, BiComment } from "react-icons/bi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Post(props) {
   console.log(props);
   const name = props.groupName;
+  const navigate = useNavigate();
   const groupId = props.groupId;
   const postId = props.postId;
   const username = props.username;
@@ -18,10 +19,12 @@ export default function Post(props) {
 
   const title = props.title;
   const body = props.caption;
-
+  const commentHanlder = ()=>{
+    navigate(`/posts/${id}`)
+  }
   
   return (
-    <Link to= {`/posts/${id}`}>
+    
       <div className="mx-auto border-b-2 border-diffused bg-white my-2 rounded-lg p-2 ">
         <div className="postHeader flex items-center justify-between p-1">
           <div className="postInfo flex items-center gap-3">
@@ -33,7 +36,7 @@ export default function Post(props) {
               </div>
             </div>
           </div>
-          <PostDropdown />
+          <PostDropdown username={username} postId ={postId} groupId = {groupId}/>
         </div>
         <div className="postBody p-1">
           <div className="title leading-tight py-2 font-medium text-lg">
@@ -45,11 +48,11 @@ export default function Post(props) {
           <div className="py-3 rounded-xl flex justify-center items-center flex-1 hover:bg-diffused">
             <BiLike />
           </div>
-          <div className="py-3 rounded-xl flex justify-center items-center flex-1 hover:bg-diffused">
+          <div className="py-3 rounded-xl flex justify-center items-center flex-1 hover:bg-diffused" onClick={commentHanlder}>
             <BiComment />
           </div>
         </div>
       </div>
-    </Link>
+    
   );
 }
